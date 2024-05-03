63f8286edfb032bf0a6aa6e136d0274237601028
Michiel de Jong <michiel@pondersource.com>
2024-05-03 23:20:31 +0200

    returning last transaction details as headers

--- alice.mjs
+++ alice.mjs
@@ -1,14 +1,16 @@
-import { startServer, sendTransaction } from './shared.mjs';
+import { startServer, sendTransaction, getBalance } from './shared.mjs';
 
 async function run() {
-  const serverPromise = startServer("alice");
-  sendTransaction({
-    "sender": "bob",
+  // const serverPromise = startServer("alice");
+  await sendTransaction({
+    "sender": "alice",
     "recipient": "bob",
     "amount": 5,
     "description": "payment for your invoice #5398753"
   });
-  return serverPromise;
+  const balance = await getBalance('bob');
+  console.log(balance);
+  // return serverPromise;
 }
 // ...
 run();
--- shared.mjs
+++ shared.mjs
@@ -1,34 +1,60 @@
 import { createServer } from 'http';
-import { http_server as braidify} from 'braid-http';
+import { http_server as braidify } from 'braid-http';
 
 
 const PORTS = {
-    "alice": 9935,
-    "bob": 9936,
+  "alice": 9935,
+  "bob": 9936,
 };
 export async function startServer(actor) {
+  let balance = 0;
+  function processTransaction(transaction) {
+    if (transaction.sender === actor) {
+      balance += transaction.amount;
+    } else if (transaction.recipient === actor) {
+      balance -= transaction.amount;
+    }
+  }
   const server = createServer((req, res) => {
     // Add braid stuff to req and res
     braidify(req, res);
-    
+
     // Now use it
     if (req.subscribe) {
-
-      res.startSubscription({ onClose: _=> null })
+      res.startSubscription({ onClose: _ => null })
       // startSubscription automatically sets statusCode = 209
     } else {
       res.statusCode = 200
     }
-  
-    // Send the current version
-    res.sendUpdate({
-      version: ['greg'],
-      body: JSON.stringify({greg: 'greg'})
-    });
-    console.log('update sent');
+    if (req.method === 'POST') {
+      let body = [];
+      req
+      .on('data', chunk => {
+        // console.log('chunk', chunk.toString());
+        body.push(chunk);
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString();
+        // at this point, `body` has the entire request body stored in it as a string
+        const transaction = JSON.parse(body);
+        console.log('received transaction', transaction);
+        processTransaction(transaction);
+        // Send the current version
+        res.sendUpdate({
+          version: [balance.toString()],
+          body: JSON.stringify({ balance }),
+          'Last-Transaction-Id': transaction.id,
+          'Last-Transaction-Sender': transaction.sender,
+          'Last-Transaction-Recipient': transaction.recipient,
+          'Last-Transaction-Amount': transaction.amount.toString(),
+          'Last-Transaction-Description': transaction.description,
+        });
+        console.log('update sent');
+      });
+    }
   });
-  server.listen(PORTS[actor]);
   console.log(`Server for ${actor} started on port ${PORTS[actor]}`);
+  server.listen(PORTS[actor]);
 }
 
 export async function sendTransaction(transaction) {
@@ -39,9 +65,30 @@ export async function sendTransaction(transaction) {
   const res = await fetch(`http://localhost:${recipientPort}/`, {
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(transaction),
   });
-  console.log(`Transaction sent to ${transaction.recipient}, ${res.status} ${res.statusText}`);
+  const body = await res.json();
+  const headers = res.headers.entries();
+  let header = headers.next();
+  while (!header.done){
+    console.log(header.value);
+    header = headers.next();
+  }
+  console.log(`Transaction sent to ${transaction.recipient}, ${res.status} ${res.statusText}`, body);
+}
+
+export async function getBalance(actor) {
+  const recipientPort = PORTS[actor];
+  if (!recipientPort) {
+    throw new Error(`Recipient ${actor} not found`);
+  }
+  const res = await fetch(`http://localhost:${recipientPort}/`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return res.json();
 }
